3304307b669e5677275c48b5412926bfce1ead21
sara-02 <sarahmasud02@gamil.com>
2017-09-28 23:15:42 +0530

    Change item pipeline to dict

--- stack/stack/settings.py
+++ stack/stack/settings.py
@@ -16,7 +16,7 @@ BOT_NAME = 'stack'
 SPIDER_MODULES = ['stack.spiders']
 NEWSPIDER_MODULE = 'stack.spiders'
 
-ITEM_PIPELINES = ['stack.pipelines.MongoDBPipeline', ]
+ITEM_PIPELINES = {'stack.pipelines.MongoDBPipeline': 300, }
 
 MONGODB_SERVER = "localhost"
 MONGODB_PORT = 27017
